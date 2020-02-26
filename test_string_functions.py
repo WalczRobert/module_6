545254712f76eb2db6c5b4da44d4721b9ba7c0b0
rewalczak <rewalczak@gmail.com>
2020-02-25 18:59:26 -0600

    Add files via upload

---
+++ test_string_functions.py
@@ -0,0 +1,9 @@
+import more_functions.string_functions as string_func
+import unittest
+
+class nameTest(unittest.TestCase):
+    def test_multiple_string(self):
+        self.assertEqual(len(string_func.multiply_string('Billy',2)),10)
+
+if __name__=='__main__':
+    unittest.main()
