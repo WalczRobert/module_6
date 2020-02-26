e58787ede80ddc1f67f0db2c2ece1770f248251c
rewalczak <rewalczak@gmail.com>
2020-02-26 08:50:48 -0600

    Update string_functions.py

--- string_functions.py
+++ string_functions.py
@@ -1,3 +1,11 @@
+""" 
+Robert Walczak
+This multiplies the string by n.
+    :param message, a string
+    :param n, an intiger
+    :returns the message x n 
+    """
+
 
 def multiply_string(message,n):
     print(message*n)
